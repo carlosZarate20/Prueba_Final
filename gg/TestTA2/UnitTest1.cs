882389c49d4af3d49e6f3d2006af67699e70dc61
Carlos Zarate <u201314894@upc.edu.pe>
2016-05-31 20:35:19 -0500

    Primer Commit

---
+++ gg/TestTA2/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using gg;
+
+namespace TestTA2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSumaIgual()
+        {
+            Class1 clase = new Class1();
+            const int sum1 = 3;
+            const int sum2 = 2;
+            const int esperado = 5;
+
+            var actual = clase.Suma(sum1,sum2);
+
+            Assert.AreEqual(esperado, actual);
+        }
+        [TestMethod]
+        public void TestRestaIgual()
+        {
+            Class1 clase = new Class1();
+            const int res1 = 6, res2 = 2;
+            const int esperado = 5;
+            
+            var actual = clase.Resta(res1, res2);
+
+            Assert.AreNotEqual(esperado,actual);
+        }
+
+        [TestMethod]
+        public void TestBoolExiste()
+        {
+            Class1 clase = new Class1();
+            const int numero = 2;
+
+            Assert.IsTrue(clase.Existe(numero),"Es verdadero");
+        }
+
+
+        [TestMethod]
+        public void TestString()
+        {
+            Class1 clase = new Class1();
+            string p = "hola";
+            Assert.AreEqual(p, clase.Palabra());
+        }
+        [TestMethod]
+        public void TestStringMoIgual()
+        {
+            Class1 clase = new Class1();
+            string p = "hola mundo";
+            Assert.AreNotEqual(p, clase.Palabra());
+        }
+
+        [TestMethod]
+        public void TestISNOTNULL()
+        {
+            Class1 clase = new Class1();
+
+            Assert.IsNotNull(clase.RetornaLista());
+        }
+    }
+}
---
+++ gg/gg/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gg
+{
+    public class Class1
+    {
+           public int Suma(int a, int b)
+            {
+                return a + b;
+            }
+    
+        public int Resta (int a, int b){
+           return a - b;
+        }
+
+        public bool Existe(int a)
+        {
+            int[] vector = new int[3];
+
+            for (int i = 0; i < 3;i++ )
+            {
+                vector[i] = i + 1;
+            }
+
+            for (int i = 0; i < 3; i++ )
+            {
+                if (vector[i] == a)
+                    return true;
+            }
+            return false;
+        }
+
+        public string Palabra()
+        {
+            string pa = "hola";
+            return pa;
+        }
+
+        public List<int> RetornaLista()
+        {
+            List<int> lista = new List<int>();
+
+            lista.Add(1);
+            lista.Add(2);
+            lista.Add(3);
+
+            return lista;
+        }
+
+    }
+}
